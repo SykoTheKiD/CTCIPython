1bd2f3978d5087fc814e25b08f8e1240423ccf45
Jay SyKo <jay@jaysyko.com>
2017-06-04 22:59:12 -0400

    Added more

--- trees_and_graphs.py
+++ trees_and_graphs.py
@@ -6,10 +6,18 @@ class Node():
 	def __init__(self, data):
 		self.data = data
 		self.connections = []
-		self.visited = False
 
 	def connect(self, node):
 		self.connections.append(node)
+
+class Binary_Node():
+	def __init__(self, data):
+		self.data = data
+		self.left = None
+		self.right = None
+
+	def __str__(self):
+		return str(self.data)
 
 def route_between_nodes(node1, node2):
 	queue = Queue()
@@ -23,7 +31,30 @@ def route_between_nodes(node1, node2):
 				queue.put(each_node)
 	return False
 
+def array_to_tree(arr):
+	if len(arr) == 0:
+		return None
+	elif len(arr) == 1:
+		return Binary_Node(arr[0])
+	else:
+		mid_index = len(arr) // 2
+		mid = arr[mid_index]
+		root = Binary_Node(mid)
+		root.left = array_to_tree(arr[:mid_index])
+		root.right = array_to_tree(arr[mid_index:])
+		return root
+
+def print_tree(tree):
+	if tree == None:
+		return None
+	else:
+		print(tree.data)
+		print_tree(tree.left)
+		print_tree(tree.right)
+
 def main():
+	# Route between two nodes
+	print("Route between two nodes\n\n")
 	a = Node('a')
 	b = Node('a')
 	c = Node('a')
@@ -38,5 +69,10 @@ def main():
 	d.connections.append(b)
 	print(route_between_nodes(a, f))
 
+	# Minimal Tree
+	print("Minimal Tree\n")
+	arr = [1,2,3,4,5,6]
+	tree = array_to_tree(arr) 
+	print_tree(tree)
 if __name__ == "__main__":
 	main()
